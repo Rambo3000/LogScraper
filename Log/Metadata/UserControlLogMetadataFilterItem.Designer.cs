7f2b077d8fdcd37f2edeadd861bd038a83e00edc
Robert <robertdevolder@gmail.com>
2025-03-24 11:19:18 +0100

    Filter checkboxes sizing improved. Performance of filtering improved

--- Log/Metadata/UserControlLogMetadataFilterItem.Designer.cs
+++ Log/Metadata/UserControlLogMetadataFilterItem.Designer.cs
@@ -13,10 +13,9 @@ namespace LogScraper
             // 
             // CheckBoxItem
             // 
-            CheckBoxItem.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             CheckBoxItem.Location = new System.Drawing.Point(0, 0);
             CheckBoxItem.Name = "CheckBoxItem";
-            CheckBoxItem.Size = new System.Drawing.Size(134, 19);
+            CheckBoxItem.Size = new System.Drawing.Size(93, 19);
             CheckBoxItem.TabIndex = 0;
             CheckBoxItem.Text = "Text";
             CheckBoxItem.UseVisualStyleBackColor = true;
@@ -25,23 +24,25 @@ namespace LogScraper
             // LabelCount
             // 
             LabelCount.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            LabelCount.Location = new System.Drawing.Point(127, 1);
+            LabelCount.AutoSize = true;
+            LabelCount.Location = new System.Drawing.Point(130, 1);
             LabelCount.Margin = new Padding(0);
             LabelCount.Name = "LabelCount";
-            LabelCount.Size = new System.Drawing.Size(47, 18);
+            LabelCount.Size = new System.Drawing.Size(43, 15);
             LabelCount.TabIndex = 1;
             LabelCount.Text = "999999";
             LabelCount.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // UserControlLogMetadataFilterItem
             // 
-            Controls.Add(CheckBoxItem);
             Controls.Add(LabelCount);
+            Controls.Add(CheckBoxItem);
             Margin = new Padding(0);
             Name = "UserControlLogMetadataFilterItem";
             Size = new System.Drawing.Size(170, 19);
             SizeChanged += LogMetadataFilterItem_SizeChanged;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         private System.Windows.Forms.CheckBox CheckBoxItem;
